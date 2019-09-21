460abf9e585379a6e804685b03a3bc5ded1432fa
oi <onno.invernizzi@ivention.nl>
2019-09-21 17:07:47 +0200

    Added paddle reducer tests

--- src/Tests/PaddleReducer.test.ts
+++ src/Tests/PaddleReducer.test.ts
@@ -1,13 +1,78 @@
 import "jest";
+import { Paddle } from "../Definitions/Paddle";
 import { paddleReducer } from "../Reducers/PaddleReducer";
 import { GameActions } from "../State/GameActions";
+import { getGameDimensions } from "../GameDimensions";
 
 describe("PaddleReducer tests", () => {
     it("initializes", () => {
         // Act
-        const paddle = paddleReducer(undefined, { type: GameActions.initialize});
+        const paddle = paddleReducer(undefined, { type: GameActions.initialize });
 
         // Arrange
         expect(paddle).toBeDefined();
+    });
+
+    it("can move", () => {
+        // Arrange
+        const paddle: Paddle = {
+            color: "white",
+            height: 10,
+            isPaddle: true,
+            left: 50,
+            top: 10,
+            width: 50
+        };
+
+        const mouseXPosition = 60;
+
+        // Act
+        const result = paddleReducer(paddle, { type: GameActions.paddleMove, payload: mouseXPosition });
+
+        // Assert
+        const expectedResult = mouseXPosition - (paddle.width / 2);
+        expect(result.left).toBe(expectedResult);
+    });
+
+    it("wont set the paddle beyond the game left sizd", () => {
+        // Arrange
+        const paddle: Paddle = {
+            color: "white",
+            height: 10,
+            isPaddle: true,
+            left: 50,
+            top: 10,
+            width: 50
+        };
+
+        const mouseXPosition = 0;
+
+        // Act
+        const result = paddleReducer(paddle, { type: GameActions.paddleMove, payload: mouseXPosition });
+
+        // Assert
+        expect(result.left).toBe(0);
+    });
+
+    it("wont set the paddle beyond the game right side", () => {
+        // Arrange
+        const paddle: Paddle = {
+            color: "white",
+            height: 10,
+            isPaddle: true,
+            left: 50,
+            top: 10,
+            width: 50
+        };
+
+        const mouseXPosition = 5000;
+
+        // Act
+        const result = paddleReducer(paddle, { type: GameActions.paddleMove, payload: mouseXPosition });
+
+        const expectedValue = paddle.left - paddle.width;
+
+        // Assert
+        expect(result.left).not.toBe(expectedValue);
     });
 });
