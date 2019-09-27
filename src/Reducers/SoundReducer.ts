019c08bb981b01addbfb7a0d0cc5efd573a7a275
oi <onno.invernizzi@ivention.nl>
2019-09-27 19:56:58 +0200

    Added documentation to sound reducer.

--- src/Reducers/SoundReducer.ts
+++ src/Reducers/SoundReducer.ts
@@ -4,6 +4,13 @@ import { Bounce, HitBlock } from "../Constants/Base64Audio";
 import { SoundState } from "../State/Definition/SoundState";
 import { GameActions } from "../State/GameActions";
 
+/**
+ * Sound reducer. Handlers playing the sounds of the game when an action requires a sound.
+ * @param {SoundState} state. The sound state. Never changes after initialization.
+ * @param {GameActions} action. An action that requires a sound to be played.
+ * @returns {SoundState}. The sound state.
+ */
+
 export const soundReducer = (state: SoundState = getNewState(), action: Action): SoundState => {
     switch (action.type) {
         case GameActions.hitBlock:
